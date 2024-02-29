7edb2e6c0e1c9f4d2b1ac2efd1eba3b05e217b77
taranvd <apple.vlad.taran@gmail.com>
2024-02-29 17:24:02 +0200

    feat: Uploadthing setup

--- components/modals/initial-modals.tsx
+++ components/modals/initial-modals.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -22,7 +23,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { useEffect, useState } from "react";
+import FileUpload from "@/components/file-uploads";
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -50,9 +51,8 @@ const InitialModals = () => {
 
   const isLoading = form.formState.isSubmitting;
 
-  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+  const onSubmit = async (values: z.infer<typeof formSchema>) =>
     console.log(values);
-  };
 
   if (!isMounted) {
     return null;
@@ -77,7 +77,21 @@ const InitialModals = () => {
           >
             <div className="space-y-8 px-6">
               <div className="flex items-center justify-center text-center">
-                TODO: Image Upload
+                <FormField
+                  control={form.control}
+                  name="imageUrl"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <FileUpload
+                          endpoint="serverImage"
+                          value={field.value}
+                          onChange={field.onChange}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
               </div>
 
               <FormField
