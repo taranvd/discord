f1d6855ce2dbb3e7ebea0c38334820365801236c
taranvd <apple.vlad.taran@gmail.com>
2024-03-01 18:41:02 +0200

    feat: added Server Sidebar

---
+++ components/server/server-member.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ServerMember = () => {
+  return <div></div>;
+};
+
+export default ServerMember;
---
+++ components/server/server-search.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ServerChannel = () => {
+  return <div></div>;
+};
+
+export default ServerChannel;
