026a40665ef8e789e0c39fda2bac223430e8d1ea
taranvd <apple.vlad.taran@gmail.com>
2024-03-01 16:26:53 +0200

    feat: create Server modal

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -19,6 +19,7 @@ export function ModeToggle() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button
+          className="bg-transparent border-0"
           variant="outline"
           size="icon"
         >
