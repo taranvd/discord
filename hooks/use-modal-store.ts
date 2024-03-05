2dcf43de3cdba84b8caa91dddd6fbfc0b9b0e9a9
taranvd <apple.vlad.taran@gmail.com>
2024-03-05 03:31:28 +0200

    feat: added manage members

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,7 +1,7 @@
 import { Server } from "@prisma/client";
 import { create } from "zustand";
 
-export type ModalType = "createServer" | "invite" | "editServers";
+export type ModalType = "createServer" | "invite" | "editServers" | "members";
 
 interface ModalData {
   server?: Server;
