20ecf250e0bb6b267ca201b1e2efc2ace72751e6
taranvd <apple.vlad.taran@gmail.com>
2024-03-05 16:16:08 +0200

    feat: Delete & Leave server modal

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -6,7 +6,9 @@ export type ModalType =
   | "invite"
   | "editServers"
   | "members"
-  | "createChannel";
+  | "createChannel"
+  | "leaveServer"
+  | "deleteServer";
 
 interface ModalData {
   server?: Server;
